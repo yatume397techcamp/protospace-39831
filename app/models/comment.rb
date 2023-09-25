2be597bb940cee8cda07a45e7db1dc0f0f47ce29
yatume397techcamp <yatume397techcamp@gmail.com>
2023-09-25 18:28:50 +0900

    バリデーションを設定しましょう

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :prototype
 
-  # validates :content, presence: true      # コンセプトの名称(必須)
-
+  validates :content, presence: true   # コンセプトの名称(必須)
+  validates :prototype, presence: true # prototypeカラムのバリデーション
+  validates :user, presence: true      # userカラムのバリデーション
 
 end

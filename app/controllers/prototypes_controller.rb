d409ec69ba2cced1378d24f6831bf4344116b11b
yatume397techcamp <yatume397techcamp@gmail.com>
2023-09-25 13:25:51 +0900

    アクションとルーティングを設定しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,6 +23,14 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+  
+  end
+
+  def update
+  
+  end
+
   private
 
   def prototype_params

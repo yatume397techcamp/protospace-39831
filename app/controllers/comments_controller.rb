09449c4f72a99d95de4e1aa6381b922b286a2345
yatume397techcamp <yatume397techcamp@gmail.com>
2023-09-26 13:31:06 +0900

    コメント投稿機能を実装しましょう

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,27 @@
 class CommentsController < ApplicationController
 
   def create
+    @comment = Comment.new(comment_params)
+    binding.pry
+    # @user = curret_user
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  def show
+    # prototypesコントローラーのshowアクションに、
+    #@commentというインスタンス変数を定義し、Commentモデルの新規オブジェクトを代入した
+    @comment = Comment.new 
+    @prototype = Prototype.new
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 
 end
